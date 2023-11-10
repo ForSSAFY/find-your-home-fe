f8e0f57b27d2259fbea008136cc009aa97880494
main-character-coding <yerimin1999@gmail.com>
2023-11-10 22:10:32 +0900

    "main-search 여백 추가"

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -16,8 +16,8 @@ const cards = [
         <h1>Find Your House</h1>
         <p>우리는 당신이 편하게 쉴 수 있는 집을 찾아 주고 싶습니다.</p>
         <br />
-        <div class="main-search p-4">
-          <v-text-field class="pa-1 " placeholder="검색어를 입력해주세요" hide-details="auto"></v-text-field>
+        <div class="main-search pa-2">
+          <v-text-field class="main-search-input" placeholder="검색어를 입력해주세요" hide-details="auto"></v-text-field>
           <v-btn icon><svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="4"/><path d="M13 4.069V2h-2v2.069A8.01 8.01 0 0 0 4.069 11H2v2h2.069A8.008 8.008 0 0 0 11 19.931V22h2v-2.069A8.007 8.007 0 0 0 19.931 13H22v-2h-2.069A8.008 8.008 0 0 0 13 4.069zM12 18c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6z"/></svg></v-btn>
           <v-btn>검색</v-btn>
         </div>
@@ -81,8 +81,12 @@ img {
   justify-content: center;
   align-items: center;
   color: black;
+  margin: auto;
 }
 
+.main-search-input{
+  background-color:white;
+}
 .main-news {
   text-align: center;
 }
