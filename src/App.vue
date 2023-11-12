6dc82a80c1532dd70ce902e5ac6b6a961eacc0bb
Lechros <0onionion0@gmail.com>
2023-11-12 15:02:23 +0900

    chore(deps): vue-kakao-maps 추가

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
-import KakaoMap from '@/components/KakaoMap.vue'
+import { ref } from "vue";
+import { InfoWindow, KakaoMap, Marker, useKakaoLoader } from "vue-kakao-maps";
+
+setTimeout(() => useKakaoLoader({ appKey: import.meta.env.VITE_KAKAO_JAVASCRIPT_APP_KEY }), 100)
+const open = ref(false)
 </script>
 
 <template>
   <div>
-    <KakaoMap :center="[33.5, 126.5]" style="height: 100vh" />
+    <KakaoMap :center="{ lat: 37.5013, lng: 127.0395 }" style="width: 100%; height: 100vh;">
+      <Marker :position="{ lat: 37.5013, lng: 127.0395 }" clickable @mouseover="() => open = true"
+        @mouseout="() => open = false">
+        <InfoWindow :open="open">
+          <div style="width: 150px; padding: 5px">
+            여기는 멀티캠퍼스 역삼
+          </div>
+        </InfoWindow>
+      </Marker>
+    </KakaoMap>
   </div>
 </template>
 
