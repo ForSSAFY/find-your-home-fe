6f4045241c70655613f8496f2dcaa7faf4593063
김예림 <jin_in_1992@naver.com>
2023-11-14 11:22:22 +0900

    style: v-btn eleavation 0으로 바꿈

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -26,8 +26,8 @@ const notices = [
             <br />
           </div>
           <div class="main-video-search pa-2">
-            <v-text-field class="main-search-input" variant="solo" placeholder="검색어를 입력해주세요" hide-details="auto"></v-text-field>
-            <v-btn icon><svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="4"/><path d="M13 4.069V2h-2v2.069A8.01 8.01 0 0 0 4.069 11H2v2h2.069A8.008 8.008 0 0 0 11 19.931V22h2v-2.069A8.007 8.007 0 0 0 19.931 13H22v-2h-2.069A8.008 8.008 0 0 0 13 4.069zM12 18c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6z"/></svg></v-btn>
+            <v-text-field  class="main-search-input" variant="solo" placeholder="검색어를 입력해주세요" hide-details="auto"></v-text-field>
+            <v-btn icon :elevation="0"><svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="4"/><path d="M13 4.069V2h-2v2.069A8.01 8.01 0 0 0 4.069 11H2v2h2.069A8.008 8.008 0 0 0 11 19.931V22h2v-2.069A8.007 8.007 0 0 0 19.931 13H22v-2h-2.069A8.008 8.008 0 0 0 13 4.069zM12 18c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6z"/></svg></v-btn>
             <v-btn>검색</v-btn>
           </div>
         </div>
@@ -91,7 +91,8 @@ video {
   color: black;
   margin: auto;
   gap: 0.5rem;
-  width: 45vw;
+  max-width: 500px;
+  width: 100%;
   border: 1px solid #e2e0dd;
   background-color: white;
   border-radius: 8px;
