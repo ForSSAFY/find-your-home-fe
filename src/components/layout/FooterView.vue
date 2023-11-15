47b8830d06e3fb3c3ab17e6a82c8ab4831cdd61f
김예림 <jin_in_1992@naver.com>
2023-11-15 13:35:14 +0900

    style - footer 수정

--- src/components/layout/FooterView.vue
+++ src/components/layout/FooterView.vue
@@ -1,18 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-const icons = ref(['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'])
 </script>
 
 <template>
   <footer>
-    <v-footer class="text-center d-flex flex-column">
-      <div>
-        <v-btn v-for="icon in icons" :key="icon" class="mx-4" :icon="icon" variant="text"></v-btn>
-      </div>
-      <v-divider></v-divider>
+    <v-footer class=" d-flex flex-column">
       <div>©{{ new Date().getFullYear() }} — <strong>Find Your Home</strong>  | All Rights Reserved</div>
     </v-footer>
   </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
