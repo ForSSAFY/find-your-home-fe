b99156cf231805b10ab522176f0222f7a174d921
김예림 <jin_in_1992@naver.com>
2023-11-15 13:53:45 +0900

    style: footer 위치 고정

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
-import HeaderView from "@/components/layout/HeaderView.vue"
-import FooterView from "@/components/layout/FooterView.vue"
+import HeaderView from '@/components/layout/HeaderView.vue'
+import FooterView from '@/components/layout/FooterView.vue'
 </script>
 
 <template>
-    <HeaderView/>
+  <HeaderView />
+  <div class="wrapper">
     <router-view></router-view>
-    <FooterView/>
+  </div>
+  <FooterView class="footer" />
 </template>
 
 <style scoped>
+.wrapper {
+  height: auto;
+  min-height: 96vh;
 
+}
+.footer {
+  height: 2vh;
+  position: relative;
+  transform: translateY(-100%);
+  text-align: center;
+}
 </style>
