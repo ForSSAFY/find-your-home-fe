57dd84cbcb5b8759edbd019513608f438ca50da0
김예림 <jin_in_1992@naver.com>
2023-11-17 15:57:34 +0900

    add - login 화면 완료

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,56 +7,105 @@ const visible = ref(false)
 <template>
   <v-main>
     <div class="container">
-      <h1 class="pt-5 pb-5">로그인</h1>
-
-      <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
-        <div class="text-subtitle-1 text-medium-emphasis">ID</div>
-
-        <v-text-field
-          density="compact"
-          placeholder="아이디"
-          prepend-inner-icon="person"
-          variant="outlined"
-        ></v-text-field>
-
-        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-          Password
-          <a
-            class="text-caption text-decoration-none"
-            href="#"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            비밀번호를 잊으셨나요?</a
-          >
+      <v-card class="login-form" elevation="8" rounded="0">
+        <div class = "login-form-inner">
+          <div class="header mb-3">
+            <h2>로그인</h2>
+            <div class="header-line">
+            </div>
+          </div>
+  
+          <v-text-field
+            density="comfortable"
+            placeholder="아이디"
+            prepend-inner-icon="person"
+            variant="outlined"
+          ></v-text-field>
+  
+          <v-text-field
+            :append-inner-icon="visible ? 'visibility_off' : 'visibility'"
+            :type="visible ? 'text' : 'password'"
+            density="comfortable"
+            placeholder="비밀번호"
+            prepend-inner-icon="lock"
+            variant="outlined"
+            @click:append-inner="visible = !visible"
+          ></v-text-field>
+  
+          <v-btn class="login-button" rounded="0">로그인</v-btn>
+  
+          <v-card-text class="find_wrap">
+            <router-link to="/join" class="text-decoration-none">아이디 찾기</router-link>
+            <v-divider inset vertical></v-divider>
+            <router-link to="/join" class="text-decoration-none">비밀번호 찾기</router-link>
+            <v-divider inset vertical></v-divider>
+            <router-link to="/join" class="text-decoration-none">회원 가입</router-link>
+          </v-card-text>
         </div>
-
-        <v-text-field
-          :append-inner-icon="visible ? 'visibility_off' : 'visibility'"
-          :type="visible ? 'text' : 'password'"
-          density="compact"
-          placeholder="비밀번호"
-          prepend-inner-icon="lock"
-          variant="outlined"
-          @click:append-inner="visible = !visible"
-        ></v-text-field>
-
-        <v-btn block class="mb-1" size="large" variant="tonal">로그인</v-btn>
-
-        <v-card-text class="text-center">
-          <router-link to="/join" class="text-decoration-none">회원가입</router-link>
-        </v-card-text>
       </v-card>
     </div>
   </v-main>
 </template>
 
 <style scoped>
-.container{
-    width: 100vw;
+.container {
+  width: 100vw;
+}
+.login-form{
+  width: 800px;
+  height: 600px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.37);
 }
 
-.container h1{
-    text-align: center;
+.login-form-inner{
+  width: 580px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.header {
+  display: flex;
+  flex-direction: row;
+  align-items: baseline;
+  justify-content: space-between;
+  gap: 5px
+}
+
+.header h2{
+  text-align: left;
+  font-weight: 700;
+  font-size: 40px;
+}
+
+.header-line{
+  /* (grow, shrink, basis) */
+  flex: 1 0 0; 
+  border-bottom: 2px solid black;
+}
+
+.v-divider{
+  height: 16px;
+  background-color: black;
+}
+
+/* 로그인 버튼 */
+.login-button {
+  width: 580px;
+  height: 56px;
+  background-color: black;
+  color: white;
+}
+
+.find_wrap {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  gap: 10px;
 }
 </style>
