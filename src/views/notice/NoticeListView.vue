a98a8941fe0e467a3a06dff58f2a54108d7bad30
main-character-coding <yerimin1999@gmail.com>
2023-11-23 03:24:51 +0900

    feat : pageNavigation 구현

--- src/views/notice/NoticeListView.vue
+++ src/views/notice/NoticeListView.vue
@@ -1,21 +1,54 @@
 <script lang="ts" setup>
+import { ref, computed } from 'vue'
+
 const notices = [
   { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2023/09/21) 개정안내' },
   { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2023/08/21) 개정안내' },
   { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2023/07/21) 개정안내' },
-  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2023/06/21) 개정안내' }
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2023/06/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2023/05/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2023/04/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2023/03/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2023/02/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2023/01/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2022/12/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2022/11/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2022/10/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2022/09/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2022/08/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2022/07/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2022/06/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2022/05/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2022/04/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2022/03/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2022/02/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2022/01/21) 개정안내' },
+  { date: 20231117, title: '[공지] Find your house 개인정보처리방침 (2021/12/21) 개정안내' }
 ]
+
+const page = ref(1)
+const perPage = 10
+const totalNotice = notices.length
+const totalPage = Math.ceil(totalNotice / perPage)
+
+const displayNotices = computed(() => {
+  const start = (page.value - 1) * perPage
+  return notices.slice(start, start + perPage)
+})
 </script>
 <template>
   <v-container class="container">
-    <header class="header mb-6">
+    <header class="header">
       <h2>공지사항</h2>
       <div class="header-line"></div>
     </header>
-    <div v-for="n in notices" :key="n.title" class="notices">
+    <div v-for="n in displayNotices" :key="n.title" class="notices">
       <div>{{ n.date }}</div>
       <div>{{ n.title }}</div>
     </div>
+    <v-pagination 
+      v-model="page"
+     :length="totalPage"></v-pagination>
   </v-container>
 </template>
 <style scoped>
@@ -45,9 +78,10 @@ const notices = [
   height: 1.85rem;
 }
 
-.notices{
+.notices {
   display: flex;
   border-bottom: 2px solid black;
   gap: 20px;
+  padding: 1rem;
 }
 </style>
