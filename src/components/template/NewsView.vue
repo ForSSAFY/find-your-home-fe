1e1e5591fcfaca75a6435644e5c03ad46bb625e9
김예림 <jin_in_1992@naver.com>
2023-11-22 11:22:26 +0900

    add : new View 생성

---
+++ src/components/template/NewsView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const news = defineProps < { title: string, description: string, imgurl: string, href: string }
+>()
+</script>
+
+<template>
+  <a :href="href" class="link">
+    <v-img height="15rem" :src="news.imgurl" cover></v-img>
+
+    <v-card-title> {{ news.title }} </v-card-title>
+
+    <v-card-subtitle> {{ news.description }} </v-card-subtitle>
+  </a>
+</template>
+
+<style scoped>
+.link{
+
+}
+</style>
