c407e4d45e459635f59b58f9352ae0279cbdd446
김예림 <jin_in_1992@naver.com>
2023-11-22 11:23:58 +0900

    style : 뉴스 템플렛 수정

--- src/views/news/NewsListView.vue
+++ src/views/news/NewsListView.vue
@@ -1,20 +1,51 @@
 <script lang="ts" setup>
-
+const today = new Date()
+const date = today.getFullYear() + '년 ' + (today.getMonth() + 1) + '월 ' + today.getDate() + '일'
 </script>
 
 <template>
-    <v-container>
-    <div>오늘의 뉴스</div>
-</v-container>
+  <v-container class="container">
+    <div class="header">
+      <h2>오늘의 뉴스</h2>
+      <div class="header-more-box">
+        <div>{{ date }}</div>
+      </div>
+    </div>
+    
+  </v-container>
 </template>
 
 <style scoped>
-    .container{
-        max-width: 1200px;
-        width: 100%;
-        min-width: 100vh;
-        margin: 0 auto;
-        display: flex;
-        flex-direction: column;
-    }
+.container {
+  max-width: 1200px;
+  width: 100%;
+  flex-direction: column;
+  padding: 0 1rem;
+  margin: 0 auto;
+  display: flex;
+}
+
+/* 헤더 */
+.header {
+  padding-top: 2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: baseline;
+  justify-content: space-between;
+  gap: 16px;
+}
+
+.header h2 {
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 1;
+}
+
+.header-more-box {
+  /* (grow, shrink, basis) */
+  display: flex;
+  justify-content: end;
+  flex: 1 0 0;
+  border-bottom: 2px solid black;
+}
 </style>
